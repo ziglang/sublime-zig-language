76366bd61b3074a446b7d173685fc5f067aea519
Jeff Fowler <jeffowler@gmail.com>
2018-01-25 13:50:49 +0100

    add samples.zig

---
+++ samples.zig
@@ -0,0 +1,58 @@
+// this is a syntactically meaningless collection of sample
+// keywords and constructs to use in testing.
+
+const var extern packed export pub noalias inline comptime nakedcc stdcallcc volatile align section
+
+union
+struct
+enum
+
+break return continue asm defer errdefer unreachable
+
+if else switch and or
+
+while for
+
+null undefined this
+
+fn use test
+
+bool f32 f64 f128 void noreturn type error
+
+i2 u2 i3 u3 i4 u4 i5 u5 i6 u6 i7 u7 i8 u8 i16 u16 u29 i29 i32 u32 i64 u64 i128 u128 isize usize
+
+c_short c_ushort c_int c_uint c_long c_ulong c_longlong c_ulonglong c_longdouble c_void
+
+true false
+
+==
+!=
+and or
+- -?  + +?  * *? / /*
+<< <<? >> ! & ^ |
+== ++ ** ->
+
+123
+123.123
+123.123e123
+123.123E123
+
+0x123
+0x123.123
+0x123.123p123
+
+0o123
+
+0b1
+
+/// TODO blah blah
+// TODO blah blah
+
+"adsfjioasdjfoiad"
+c"adsfjioasdjfoiad"
+
+\\ adsjfafsdjkl
+\\ adsjfafsdjkl
+\\ adsjfafsdjkl
+\\ adsjfafsdjkl
+\\ adsjfafsdjkl
