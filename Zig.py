ecf2bb1c879fb65bf4411ee935f9883f6b423711
emekoi <emekankurumeh@outlook.com>
2019-07-07 14:56:31 -0500

    fixed broken settings menu and added config options

--- Zig.py
+++ Zig.py
@@ -1,14 +1,24 @@
 import sublime
 import sublime_plugin
 
-def fmt(fmt, dic):
-  for k in dic:
-    fmt = fmt.replace("{" + k + "}", str(dic[k]))
-  return fmt
+settings = sublime.load_settings('Zig.sublime-settings')
+
+
+def get_setting(view, opt, default):
+    return view.settings().get(opt, settings.get(opt, default))
+
 
 class Zig(sublime_plugin.EventListener):
     def on_post_save_async(self, view):
-        global_settings = sublime.load_settings(fmt("{name}.sublime-settings", { "name": self.__class__.__name__ }))
-        should_fmt = view.settings().get('fmt.on_save.enabled', global_settings.get('fmt.on_save.enabled'))
-        if (should_fmt):
-            view.window().run_command("build", {"variant": "Format File"})
+        sel = view.sel()[0]
+        region = view.word(sel)
+        scope = view.scope_name(region.b)
+        if scope.find('source.zig') != -1:
+            should_fmt = get_setting(view, 'zig.fmt.on_save', True)
+            should_build = get_setting(view, 'zig.build.on_save', False)
+
+            if (should_fmt and not should_build):
+                mode = get_setting(view, 'zig.fmt.mode', 'File').title()
+                view.window().run_command('build', {'variant': 'Format ' + mode})
+            elif (should_build and not should_fmt):
+                view.window().run_command('build')
